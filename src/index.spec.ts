9544d3835e7c93a6af77ce685015428e9f3062ac
harrygr <harry@grumbar.me>
2018-05-01 20:25:31 +0100

    Finalise simplification of validator

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,189 +1,54 @@
-import makeValidator, { RuleSet, Messages } from './index'
-import { Rule } from './rules'
+import { makeValidator, Constraint } from './'
+
+const equals = <T>(req: T, val: T) =>
+  val !== req ? 'Should be equal' : undefined
+const required = <T>(val: T) => (val ? undefined : 'Required')
+const atLeast18 = (val: number) => (val >= 18 ? undefined : 'Not old enough')
 
 describe('Basic validator usage', () => {
-  const validate = makeValidator()
+  it('validates a number', () => {
+    const ageValidator = makeValidator({ age: [atLeast18] })
 
-  it('consolidates the validation result when 2 fields fail', () => {
-    const data = {
-      size1: 10,
-      size2: 13,
-    }
-
-    const rules = {
-      size1: { min: 20 },
-      size2: { min: 20 },
-    }
-
-    const result = validate(rules, data)
-    expect(result.passes).toBe(false)
-    expect(result.errors).toHaveLength(2)
-    expect(result.errors).toContain('Size 2 must be greater than 20')
+    expect(
+      ageValidator({
+        age: 16,
+      }),
+    ).toEqual({ age: 'Not old enough' })
+    expect(
+      ageValidator({
+        age: 44,
+      }),
+    ).toEqual({})
   })
 
-  it('consolidates the validation result when 1 field fails', () => {
-    const data = {
-      size1: 10,
-      size2: 30,
-    }
+  it('only returns a message for the first failing contrait for a field', () => {
+    const ageValidator = makeValidator({ age: [required, atLeast18] })
 
-    const rules = {
-      size1: { min: 20 },
-      size2: { min: 20 },
-    }
-
-    const result = validate(rules, data)
-    expect(result.passes).toBe(false)
-    expect(result.errors).toHaveLength(1)
-    expect(result.errors).toContain('Size 1 must be greater than 20')
+    expect(
+      ageValidator({
+        age: undefined,
+      }),
+    ).toEqual({ age: 'Required' })
   })
 
-  it('consolidates the validation result when no fields fail', () => {
-    const data = {
-      size1: 40,
-      size2: 30,
-    }
-
-    const rules = {
-      size1: { min: 20 },
-      size2: { min: 20 },
-    }
-
-    const result = validate(rules, data)
-    expect(result.passes).toBe(true)
-    expect(result.errors).toHaveLength(0)
-  })
-
-  it('validates for existance', () => {
-    const data = {
-      name: 'Joe Bloggs',
-      place: '',
-    }
-
-    const rules = {
-      name: { required: true },
-      place: { required: true },
-    }
-
-    const result = validate(rules, data)
-
-    expect(result.fields.name.passes).toBe(true)
-    expect(result.fields.place.passes).toBe(false)
-    expect(result.fields.place.errors).toHaveLength(1)
-    expect(result.fields.place.errors[0]).toEqual('Place is required')
-  })
-
-  it("validates the size of something that's big enough", () => {
-    const data = { age: 27 }
-    const rules = { age: { min: 18 } }
-
-    const result = validate(rules, data)
-    expect(result.fields.age.passes).toBe(true)
-  })
-
-  it("validates the size of something that's too small", () => {
-    const data = { age: 16 }
-    const rules = { age: { min: 18 } }
-
-    const result = validate(rules, data)
-    expect(result.fields.age.passes).toBe(false)
-    expect(result.fields.age.errors[0]).toBe('Age must be greater than 18')
-  })
-})
-
-describe('Adding custom rules', () => {
-  const rules = {
-    isAwesome: (_val, req) => req,
+  interface PasswordFields {
+    password: string
+    passwordConfirm: string
   }
-  const validate = makeValidator({ rules })
 
-  const data = {
-    age: 21,
-    name: 'Bob',
-  }
-  it('allows custom rules to be added', () => {
-    const result = validate(
-      {
-        age: { required: true },
-        name: { isAwesome: true },
-      },
-      data,
-    )
+  it('validates a field being equal to another', () => {
+    const passwordEquals: Constraint<PasswordFields> = (val, fields) =>
+      equals(fields.passwordConfirm, val)
 
-    expect(result.fields.name.passes).toBe(true)
-  })
-
-  it('works when a custom rule fails', () => {
-    const result = validate(
-      {
-        age: { required: true },
-        name: { isAwesome: false },
-      },
-      data,
-    )
-
-    expect(result.fields.name.passes).toBe(false)
-    expect(result.fields.name.errors[0]).toContain('"Name" failed')
-  })
-})
-
-describe('Adding custom messages', () => {
-  it('allows setting a message for a custom rule', () => {
-    const validate = makeValidator({
-      rules: {
-        containsWord: (val: string, word: string) => val.indexOf(word) > -1,
-      },
-      messages: {
-        containsWord: (field, word) =>
-          `${field} must contain the word "${word}"!`,
-      },
+    const passwordValidator = makeValidator<PasswordFields>({
+      password: [required, passwordEquals],
     })
 
-    const result = validate(
-      { description: { containsWord: 'foo' } },
-      { description: 'hello, bar' },
-    )
-    expect(result.fields.description.errors).toContain(
-      'Description must contain the word "foo"!',
-    )
-  })
-
-  it('allows overriding a default message', () => {
-    const validate = makeValidator({
-      rules: {},
-      messages: {
-        required: (field, req) => `${field} must be provided yo!`,
-      },
+    const result = passwordValidator({
+      password: 'foo',
+      passwordConfirm: 'bar',
     })
 
-    const result = validate({ name: { required: true } }, { name: '' })
-    expect(result.fields.name.errors).toContain('Name must be provided yo!')
-  })
-
-  it('allows adding a custom message for a specific field', () => {
-    const validate = makeValidator({
-      messages: { min: () => 'Too short' },
-    })
-
-    const result = validate(
-      {
-        name: { min: 10 },
-        faveAnimal: { min: 5 },
-      },
-      {
-        name: 'Bobby',
-        faveAnimal: 'Cat',
-      },
-      {
-        faveAnimal: {
-          min: (field, req) => `Fave animal short be at least ${req} chars`,
-        },
-      },
-    )
-
-    expect(result.fields.name.errors).toContain('Too short')
-    expect(result.fields.faveAnimal.errors).toContain(
-      'Fave animal short be at least 5 chars',
-    )
+    expect(result).toEqual({ password: 'Should be equal' })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,37 @@
-import unsluggify from './utils/unsluggify'
-import defaultRules, { DefaultRules, Rule } from './rules'
-
-export type RuleSet<R> = Partial<Record<keyof DefaultRules | keyof R, any>>
-
-export type MessageFunc = (fieldName: string, req: any) => string
-export type DefaultMessages = Record<keyof DefaultRules, MessageFunc>
-export type Messages<R> = Partial<
-  Record<keyof R | keyof DefaultRules, MessageFunc>
+export type Constraint<F extends object, T = any> = (
+  val: T,
+  fields: F,
+) => string | undefined
+export type Constraints<F extends object> = Partial<
+  Record<keyof F, Constraint<F>[]>
+>
+export type ValidationResult<F extends object> = Record<
+  keyof F,
+  string | undefined
 >
 
-export interface ValidationResult {
-  errors: string[]
-  passes: boolean
-}
+export function makeValidator<F extends object>(constraints: Constraints<F>) {
+  return (fields: F): ValidationResult<F> => {
+    const fieldNames = Object.keys(fields) as (keyof F)[]
 
-export interface Result<D> {
-  fields: Record<keyof D, ValidationResult>
-  errors: string[]
-  passes: boolean
-}
+    function getErrors(
+      acc: ValidationResult<F>,
+      fieldName: keyof F,
+    ): ValidationResult<F> {
+      const val = fields[fieldName]
+      const fieldConstraints = (constraints[fieldName] || []) as Constraint<
+        F,
+        typeof val
+      >[]
 
-export interface Options<R> {
-  rules?: R
-  messages?: Messages<R>
-}
+      const error = fieldConstraints.reduce(
+        (err, con) => (err ? err : con(val, fields)),
+        undefined,
+      )
 
-export default function makeValidator<R>(options: Options<R> = {}) {
-  const combinedRules = { ...defaultRules, ...(options.rules || {}) }
-  const messages = { ...defaultMessages, ...((options.messages as {}) || {}) }
-
-  function getErrors(
-    field: string,
-    value: any,
-    fieldConstraints: RuleSet<R>,
-    messageOverrides: Messages<R>,
-  ) {
-    const fieldName = unsluggify(field)
-    const combinedMessages = {
-      ...messages,
-      ...((messageOverrides as {}) || {}),
+      return Object.assign({}, acc, { [fieldName]: error })
     }
 
-    return Object.keys(fieldConstraints).reduce<string[]>(
-      (prev, constraint) => {
-        if (combinedRules[constraint](value, fieldConstraints[constraint])) {
-          return prev
-        }
-        const message = combinedMessages[constraint]
-          ? combinedMessages[constraint](
-              fieldName,
-              fieldConstraints[constraint],
-            )
-          : makeFallbackMessage(fieldName, constraint)
-        return prev.concat(message)
-      },
-      [],
-    )
+    return fieldNames.reduce(getErrors, {} as ValidationResult<F>)
   }
-
-  return function validate<D>(
-    constraints: Record<keyof D, RuleSet<R>>,
-    data: D,
-    messages: Partial<Record<keyof D, Messages<R>>> = {},
-  ) {
-    const fields = Object.keys(data).reduce(
-      (prev, field) => {
-        const messageOverrides = messages[field] || {}
-        const errors = getErrors(
-          field,
-          data[field],
-          constraints[field] || {},
-          messageOverrides,
-        )
-        const fieldPasses = !errors.length
-        return {
-          ...prev,
-          errors: prev.errors.concat(errors),
-          passes: prev.passes ? fieldPasses : prev.passes,
-          fields: {
-            ...prev.fields,
-            [field]: {
-              errors,
-              passes: fieldPasses,
-            },
-          },
-        }
-      },
-      { fields: {}, passes: true, errors: [] as string[] },
-    )
-
-    return fields as Result<D>
-  }
-}
-
-function makeFallbackMessage(fieldName: string, ruleName: string) {
-  return `"${fieldName}" failed the "${ruleName}" check`
-}
-
-const defaultMessages: DefaultMessages = {
-  required: (fieldName, req) => `${fieldName} is required`,
-  min: (fieldName, min) => `${fieldName} must be greater than ${min}`,
-  max: (fieldName, max) => `${fieldName} must be smaller than ${max}`,
-  equals: (fieldName, compare) => `${fieldName} does not match`,
 }
